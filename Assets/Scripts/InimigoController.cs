31a14b69ae2fe39a010539c5da682635a224aea4
Daniel Felipe Coelho de Freitas <danielfreitaslele14@gmail.com>
2025-06-10 19:53:10 -0300

    organização dos scripts e Prefab do inimigo adicionado

---
+++ Assets/Scripts/InimigoController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class InimigoController : MonoBehaviour
+{
+    public Transform areaDisparo;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
