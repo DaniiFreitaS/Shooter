586f48c9cd194f33f7d5a0bb652676a6956b6b73
Daniel Felipe Coelho de Freitas <danielfreitaslele14@gmail.com>
2025-06-13 23:06:34 -0300

    Movimentacao dos inimigos
    Agora os inimigos se movimentam, respeitando os limites

--- Assets/Scripts/SpawnInimigos.cs
+++ Assets/Scripts/SpawnInimigos.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 
 public class SpawnInimigos : MonoBehaviour
 {
+    //grid inimigos
     public GameObject inimigoPrefab;
     public int linhas;
     public int colunas;
     public float espacamento;
     public float delaytiro;
+
+    //movimentacao grid
+    public Transform limiteEsq;
+    public Transform limiteDir;
+    public Transform limiteInf;
+    public float velocidade = 2f;
+    private int direcao = 1;
+
     private float clock;
     private List<InimigoController> inimigosVivos = new List<InimigoController>();
     private List<InimigoController> inimigosUltimaLinha = new List<InimigoController>();
@@ -24,6 +33,7 @@ public class SpawnInimigos : MonoBehaviour
             clock = delaytiro;
             Atirar();
         }
+        Movimentar();
     }
 
 
@@ -35,6 +45,7 @@ public class SpawnInimigos : MonoBehaviour
             {
                 Vector3 posicao = new Vector3(j * espacamento, 0, -i * espacamento);
                 GameObject inimigo = Instantiate(inimigoPrefab,transform.position + posicao, Quaternion.identity);
+                inimigo.transform.parent = this.transform;
                 InimigoController inimigoController = inimigo.GetComponent<InimigoController>();
                 inimigoController.posicaoLinha = i+1;
                 inimigosVivos.Add(inimigoController);
@@ -62,5 +73,25 @@ public class SpawnInimigos : MonoBehaviour
         int rand = Random.Range(0, inimigosUltimaLinha.Count);
         EnemyShooter e = inimigosUltimaLinha[rand].GetComponent<EnemyShooter>();
         e.Shoot();
+    }
+
+    void Movimentar()
+    {
+        transform.Translate(Vector3.right * direcao * velocidade * Time.deltaTime);
+
+        // Checa se algum inimigo saiu dos limites
+        foreach (var inimigo in inimigosVivos)
+        {
+            if (inimigo.transform.position.z <= limiteInf.position.z) 
+            {
+                Time.timeScale = 0f;  //para quando chega no limite inferior, trocar para tela de derrota    
+            }
+            if (inimigo.transform.position.x <= limiteEsq.position.x || inimigo.transform.position.x >= limiteDir.position.x)
+            {
+                direcao *= -1;
+                transform.position += Vector3.back * 0.5f;
+                break;
+            }
+        }
     }
 }
